9ba8dd477b9a824a34e22a7b235ea57bb32f7553
ttuan <tuantv.nhnd@gmail.com>
2024-08-24 09:22:06 +0700

    feat(next.js): Server and Client Components

---
+++ basic-js/app/like-button.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+
+  return <button onClick={() => setLikes(likes + 1)}>{likes} 👍</button>;
+}
--- basic-js/app/page.js
+++ basic-js/app/page.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import LikeButton from "./like-button";
 
 function Header(props) {
   return <h1>{props.title}</h1>;
@@ -6,12 +6,6 @@ function Header(props) {
 
 export default function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
 
   return (
     <div>
@@ -22,7 +16,7 @@ export default function HomePage() {
         ))}
       </ul>
 
-      <button onClick={handleClick}>Like ({likes})</button>
+      <LikeButton />
     </div>
   );
 }
